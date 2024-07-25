fa7edfde212446bfa39b21e4eef89eb6ab7ad62e
zeeshan038 <98173945+zeeshan038@users.noreply.github.com>
2024-07-25 17:34:44 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        origin : ["https://mern-blog-refined-frontend.vercel.app/"],
+        origin : ["https://mern-blog-refined-frontend.vercel.app"],
         methods : ["POST" , "GET"],
         credentials : true
     }
